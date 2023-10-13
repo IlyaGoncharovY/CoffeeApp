656c76c8ffded04d2dcc8a928e0065e91a9b65b7
IlyaGoncharovY <goncharov.i@rambler.ru>
2023-10-14 02:44:21 +0300

    fix (AppContainer) delete fonts logic for render. Error!!

--- src/AppContainer.tsx
+++ src/AppContainer.tsx
@@ -1,37 +1,7 @@
-import React, {useEffect, useState} from 'react';
-import * as Font from 'expo-font';
-import * as SplashScreen from 'expo-splash-screen';
+import React from 'react';
 
 import {Navigation} from './Navigation';
 
-const loadFonts = async () => {
-  await Font.loadAsync({
-    'kt-bold': require('./assects/fonts/Kanit-SemiBold.ttf'),
-    'kt-light': require('./assets/fonts/Kanit-Light.ttf'),
-  });
-};
-
 export const AppContainer = () => {
-  const [isReady, setIsReady] = useState<boolean>(false);
-
-  useEffect(() => {
-    async function prepare() {
-      try {
-        await SplashScreen.preventAutoHideAsync();
-        await loadFonts();
-        setIsReady(true);
-        await SplashScreen.hideAsync();
-      } catch (e) {
-        console.warn(e);
-      }
-    }
-
-    prepare();
-  }, []);
-
-  if (!isReady) {
-    return null;
-  }
-
   return <Navigation />;
 };
