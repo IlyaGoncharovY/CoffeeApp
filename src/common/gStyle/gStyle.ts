3aed871c96e8404a0fee5735e428f736e04d0b03
IlyaGoncharovY <goncharov.i@rambler.ru>
2023-10-14 02:46:29 +0300

    fix (gStyle) refactor styles

--- src/common/gStyle/gStyle.ts
+++ src/common/gStyle/gStyle.ts
@@ -1,17 +1,14 @@
 import {StyleSheet} from 'react-native';
 
-const gStyles = StyleSheet.create({
+export const gStyles = StyleSheet.create({
   main: {
     flex: 1,
-    padding: 20,
-    paddingTop: 60,
-    borderColor: '#55423d',
+    backgroundColor: '#55423d',
   },
   title: {
     fontSize: 25,
     color: '#fffffe',
-    fontFamily: 'kt-bold',
     textAlign: 'center',
-    marginTop: 25,
+    margin: 5,
   },
 });
