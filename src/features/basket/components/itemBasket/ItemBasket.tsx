3e8f4068b1b13816d1e36eb67e8c75a657ec5157
IlyaGoncharovY <goncharov.i@rambler.ru>
2023-10-16 19:47:36 +0300

    feat (ItemBasket) added new item for render count coffee in basket

---
+++ src/features/basket/components/itemBasket/ItemBasket.tsx
@@ -0,0 +1,55 @@
+import React, {FC} from 'react';
+import {Button, Image, StyleSheet, Text, View} from 'react-native';
+
+import {CoffeesType} from '../../../coffee/slice/CoffeeSlice';
+import {gStyles} from '../../../../common/gStyle/gStyle';
+import {useAppDispatch} from '../../../../store/config/hooks';
+import {removeCoffee} from '../../slice/BasketSlice';
+
+interface IItemBasket {
+  coffeesForBasket: CoffeesType;
+}
+
+export const ItemBasket: FC<IItemBasket> = ({coffeesForBasket}) => {
+  const dispatch = useAppDispatch();
+
+  const removeCoffeeHandler = (coffeeId: string) => {
+    dispatch(removeCoffee(coffeeId));
+  };
+
+  return (
+    <View>
+      <View style={styles.container}>
+        <Text style={gStyles.title}>{coffeesForBasket.title}</Text>
+        <Image
+          source={{
+            uri: coffeesForBasket.img,
+          }}
+          style={styles.image}
+        />
+        <Text>{coffeesForBasket.description}</Text>
+        <Text>{coffeesForBasket.count}</Text>
+        <Button
+          color={'#ffc0ad'}
+          title={'-'}
+          onPress={() => removeCoffeeHandler(coffeesForBasket.id)}
+        />
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    padding: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc',
+  },
+  image: {
+    width: 40,
+    height: 40,
+  },
+});
