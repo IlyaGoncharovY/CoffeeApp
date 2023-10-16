68af0d7e4a1d2e6813b69bad3ee2a30f5e0452aa
IlyaGoncharovY <goncharov.i@rambler.ru>
2023-10-16 19:45:10 +0300

    fix (BasketSlice) refactor logic for added new coffee in basket and added new reducer for delete coffee in basket

--- src/features/basket/slice/BasketSlice.ts
+++ src/features/basket/slice/BasketSlice.ts
@@ -1,4 +1,5 @@
 import {createSlice, PayloadAction} from '@reduxjs/toolkit';
+
 import {CoffeesType} from '../../coffee/slice/CoffeeSlice';
 
 interface initialStateType {
@@ -14,10 +15,33 @@ const basketSlice = createSlice({
   initialState,
   reducers: {
     addToBasket: (state, action: PayloadAction<CoffeesType>) => {
-      state.coffeesForBasket.push(action.payload);
+      const existingCoffee = state.coffeesForBasket.find(
+        el => el.id === action.payload.id,
+      );
+
+      if (existingCoffee) {
+        existingCoffee.count += 1;
+      } else {
+        const newCoffee = {...action.payload, count: 1};
+        state.coffeesForBasket.push(newCoffee);
+      }
+    },
+    removeCoffee: (state, action: PayloadAction<string>) => {
+      const coffeeToRemove = state.coffeesForBasket.find(
+        el => el.id === action.payload,
+      );
+      if (coffeeToRemove) {
+        if (coffeeToRemove.count > 1) {
+          coffeeToRemove.count -= 1;
+        } else {
+          state.coffeesForBasket = state.coffeesForBasket.filter(
+            el => el.id !== action.payload,
+          );
+        }
+      }
     },
   },
 });
-export const {addToBasket} = basketSlice.actions;
+export const {addToBasket, removeCoffee} = basketSlice.actions;
 
 export default basketSlice.reducer;
