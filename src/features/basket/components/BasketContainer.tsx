61c6d122472dbb6bb3684c45fb2b82e4adedc994
IlyaGoncharovY <goncharov.i@rambler.ru>
2023-10-18 04:02:58 +0300

    fix (BasketContainer) added new logic for render basket coffee

--- src/features/basket/components/BasketContainer.tsx
+++ src/features/basket/components/BasketContainer.tsx
@@ -1,14 +1,36 @@
 import React from 'react';
-import {Button, FlatList, View} from 'react-native';
-import {gStyles} from '../../../common/gStyle/gStyle';
-import {useAppSelector} from '../../../store/config/hooks';
+import {Alert, Button, FlatList, StyleSheet, View} from 'react-native';
+
 import {ItemBasket} from './itemBasket/ItemBasket';
+import {gStyles} from '../../../common/gStyle/gStyle';
+import {useAppDispatch, useAppSelector} from '../../../store/config/hooks';
 import {CoffeesType} from '../../coffee/slice/CoffeeSlice';
+import {deleteBasket} from '../slice/BasketSlice';
 
 export const BasketContainer = () => {
   const coffeesForBasket = useAppSelector(
     state => state.basket.coffeesForBasket,
   );
+
+  const dispatch = useAppDispatch();
+
+  const purchasedСoffeeCount = coffeesForBasket.map(el => el.count);
+  const purchasedСoffeeTitle = coffeesForBasket.map(el => el.title);
+
+  const orderButtonHandler = () => {
+    Alert.alert(
+      'Thank you for ordering!',
+      `${purchasedСoffeeTitle
+        .map(
+          (title, index) => purchasedСoffeeCount[index] + '- ' + title + '\n',
+        )
+        .join('')}`,
+      [{text: 'ok'}],
+    );
+    setTimeout(() => {
+      return dispatch(deleteBasket());
+    }, 2000);
+  };
 
   return (
     <View style={gStyles.main}>
@@ -19,7 +41,23 @@ export const BasketContainer = () => {
           keyExtractor={(item: CoffeesType) => item.id}
         />
       </View>
-      <Button title={'Заказать'} onPress={() => {}} />
+      <View style={styles.buttonStyle}>
+        <Button
+          disabled={purchasedСoffeeCount.length === 0}
+          color={'#ffc0ad'}
+          title={'Заказать'}
+          onPress={orderButtonHandler}
+        />
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  buttonStyle: {
+    position: 'absolute',
+    bottom: 0,
+    right: 0,
+    width: '100%',
+  },
+});
