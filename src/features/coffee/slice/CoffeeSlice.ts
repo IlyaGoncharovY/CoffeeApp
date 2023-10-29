ddf85d63590e25d96c54c6a296959d189f59d517
IlyaGoncharovY <goncharov.i@rambler.ru>
2023-10-30 01:07:28 +0300

    fix (Coffee slice) added new reducer addedNewCoffe

--- src/features/coffee/slice/CoffeeSlice.ts
+++ src/features/coffee/slice/CoffeeSlice.ts
@@ -1,4 +1,4 @@
-import {createSlice} from '@reduxjs/toolkit';
+import {createSlice, PayloadAction} from '@reduxjs/toolkit';
 import {v1} from 'react-native-uuid/dist/v1';
 
 export type CoffeesType = {
@@ -43,14 +43,25 @@ const initialState: initialStateType = {
       img: 'http://vidpoviday.com/wp-content/uploads/2016/04/d90dc819d58c6df61a48452ef80f3719.jpg',
       count: 0,
     },
+    {
+      id: v1() as string,
+      title: 'Coffee',
+      description: 'Cool Coffee',
+      img: 'http://vidpoviday.com/wp-content/uploads/2016/04/d90dc819d58c6df61a48452ef80f3719.jpg',
+      count: 0,
+    },
   ],
 };
 
 const coffeeSlice = createSlice({
   name: 'COFFEE/coffeeSlice',
   initialState,
-  reducers: {},
+  reducers: {
+    addCoffee: (state, action: PayloadAction<CoffeesType>) => {
+      state.coffees.push({...action.payload, id: v1() as string});
+    },
+  },
 });
-export const {} = coffeeSlice.actions;
+export const {addCoffee} = coffeeSlice.actions;
 
 export default coffeeSlice.reducer;
