07102d7333a442b940c7e7408b800b08a0432955
IlyaGoncharovY <goncharov.i@rambler.ru>
2023-10-29 22:13:28 +0300

    fix (useNavigationPageInApp) comment

--- src/common/assets/customHook/useNavigationPageInApp.ts
+++ src/common/assets/customHook/useNavigationPageInApp.ts
@@ -2,6 +2,11 @@ import {useEffect} from 'react';
 import {ParamListBase, useNavigation} from '@react-navigation/native';
 import {NativeStackNavigationProp} from '@react-navigation/native-stack';
 
+/**
+ * custom hook for navigation on pages in app.
+ * @param isAuth
+ * @param page
+ */
 export const useNavigationPageInApp = (isAuth: boolean, page: string) => {
   const navigation = useNavigation<NativeStackNavigationProp<ParamListBase>>();
   useEffect(() => {
