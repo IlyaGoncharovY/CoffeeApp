b10859c1c90be69fa3d4a03ba654236c8276d716
IlyaGoncharovY <goncharov.i@rambler.ru>
2023-11-01 18:30:30 +0300

    feat (search input) added search input for search coffee element

--- src/features/adminPanel/components/AdminPanelComponent.tsx
+++ src/features/adminPanel/components/AdminPanelComponent.tsx
@@ -1,15 +1,37 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {StyleSheet, TextInput} from 'react-native';
+
 import {ListCoffee} from './listCoffee/ListCoffee';
 import {useAppSelector} from '../../../store/config/hooks';
 import {useNavigationPageInApp} from '../../../common/assets/customHook/useNavigationPageInApp';
 
 export const AdminPanelComponent = () => {
   const isAuth = useAppSelector(state => state.auth.isAuth);
+  const [searchText, setSearchText] = useState<string>('');
   useNavigationPageInApp(isAuth, 'CoffeePage');
+
+  const handleSearch = (text: string) => {
+    setSearchText(text);
+  };
 
   return (
     <>
-      <ListCoffee />
+      <TextInput
+        placeholder="Search by coffee name"
+        value={searchText}
+        onChangeText={handleSearch}
+        style={styles.searchInput}
+      />
+      <ListCoffee searchText={searchText} />
     </>
   );
 };
+
+const styles = StyleSheet.create({
+  searchInput: {
+    borderBottomColor: 'black',
+    borderWidth: 2,
+    marginHorizontal: 10,
+    marginVertical: 5,
+  },
+});
--- src/features/adminPanel/components/listCoffee/ListCoffee.tsx
+++ src/features/adminPanel/components/listCoffee/ListCoffee.tsx
@@ -1,18 +1,30 @@
-import React from 'react';
-import {FlatList, View} from 'react-native';
+import React, {FC} from 'react';
+import {FlatList, Text, View} from 'react-native';
 
 import {CoffeeAdminItem} from './item/CoffeeItem';
 import {CoffeesType} from '../../../coffee/slice/CoffeeSlice';
 import {useAppSelector} from '../../../../store/config/hooks';
 import {FormComponent} from '../form/FormComponent';
 
-export const ListCoffee = () => {
+interface IListCoffee {
+  searchText: string;
+}
+
+export const ListCoffee: FC<IListCoffee> = ({searchText}) => {
   const coffees = useAppSelector(state => state.coffee.coffees);
+
+  const filteredCoffees = coffees.filter(coffee =>
+    coffee.title.toLowerCase().includes(searchText.toLowerCase()),
+  );
+
+  if (filteredCoffees.length === 0) {
+    return <Text>No matching coffees found.</Text>;
+  }
 
   return (
     <View>
       <FlatList
-        data={coffees}
+        data={filteredCoffees}
         renderItem={({item}) => <CoffeeAdminItem coffee={item} />}
         keyExtractor={(item: CoffeesType) => item.id}
         ListHeaderComponent={<FormComponent />}
