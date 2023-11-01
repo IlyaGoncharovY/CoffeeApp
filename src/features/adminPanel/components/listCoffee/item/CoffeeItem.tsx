ca23ec2925e309d3650ec89f0a68ad24f51d4edf
IlyaGoncharovY <goncharov.i@rambler.ru>
2023-11-01 18:29:49 +0300

    fix (coffee item) added logic for delete element

--- src/features/adminPanel/components/listCoffee/item/CoffeeItem.tsx
+++ src/features/adminPanel/components/listCoffee/item/CoffeeItem.tsx
@@ -2,18 +2,27 @@ import React, {FC} from 'react';
 import {Button, Text, View} from 'react-native';
 
 import {gStyles} from '../../../../../common/gStyle/gStyle';
-import {CoffeesType} from '../../../../coffee/slice/CoffeeSlice';
+import {useAppDispatch} from '../../../../../store/config/hooks';
+import {CoffeesType, deleteCoffee} from '../../../../coffee/slice/CoffeeSlice';
 
 interface ICoffeeAdminItem {
   coffee: CoffeesType;
 }
 
 export const CoffeeAdminItem: FC<ICoffeeAdminItem> = ({coffee}) => {
+  const dispatch = useAppDispatch();
+  const deleteCoffeeHandler = (coffeeId: string) => {
+    dispatch(deleteCoffee(coffeeId));
+  };
+
   return (
     <View style={gStyles.main}>
       <Text>{coffee.title}</Text>
       <Text>{coffee.description}</Text>
-      <Button title={'delete coffee'} />
+      <Button
+        title={'delete coffee'}
+        onPress={() => deleteCoffeeHandler(coffee.id)}
+      />
     </View>
   );
 };
